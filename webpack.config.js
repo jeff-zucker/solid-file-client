f949b72efceefe3d5333e20067b216eb8986f6f0
linonetwo <linonetwo012@gmail.com>
2018-12-06 18:30:36 +0800

    chore: configure build pipeline

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: './js/index.js',
+  mode: 'production',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'index.js',
+    libraryTarget: 'var',
+    library: 'pubcore',
+    libraryExport: 'default',
+  },
+};
