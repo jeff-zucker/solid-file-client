5ad94d8bb912803b467f24db52430c3b7044b5bd
A_A <21040751+Otto-AA@users.noreply.github.com>
2019-07-11 10:53:45 +0200

    Add content-type to post tests

--- tests/SolidApi.core.test.js
+++ tests/SolidApi.core.test.js
@@ -73,7 +73,8 @@ describe('core methods', () => {
       return {
         headers: {
           slug,
-          link
+          link,
+          'Content-Type': 'text/turtle'
         }
       }
     }
