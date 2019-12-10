96842f2af19e2dcc1c966e3350cb8fbfb0901fb7
Dub Zed <dubzed@gmail.com>
2019-12-09 20:12:27 -0800

    remove session and login methods

--- src/SolidFileClient.js
+++ src/SolidFileClient.js
@@ -3,52 +3,9 @@ import errorUtils from './utils/errorUtils'
 
 const { toFetchError } = errorUtils
 
-const defaultPopupUri = 'https://solid.community/common/popup.html'
-
-/**
- * @typedef {Object} SessionAuthorization
- * @property {string} client_id
- * @property {string} access_token
- * @property {string} id_token
- */
-
-/**
- * @typedef {Object} Session
- * @property {string} idp
- * @property {string} webId
- * @property {string} issuer
- * @property {string} credentialType
- * @property {string} sessionKey
- * @property {string} idClaims
- * @property {SessionAuthorization} authorization
- */
-
-/**
- * (optionally authenticated) fetch method similar to window.fetch
- * @callback fetch
- * @param {string} url
- * @param {RequestInit} [options]
- * @returns {Promise<Response>}
- */
-
-/**
- * @typedef {Object} SolidAuthClient
- * @property {fetch} fetch
- * @property {function(string, LoginOptions): Promise<Session|undefined>} login
- * @property {function({{ popupUri: string }}): Promise<Session>} popupLogin
- * @property {function(): Promise<Session>} currentSession
- * @property {function(function(Session?)): void} trackSession
- * @property {function(): Promise} logout
- */
-
 /**
  * @typedef {object} SolidFileClientOptions
  * @property {boolean|string} [enableLogging=false] - true for all logging or e.g. solid-file-client:fetch for partial logs
- */
-
-/**
- * @typedef {object} LoginCredentials
- * @todo Update this declaration
  */
 
 /**
@@ -79,85 +36,6 @@ class SolidFileClient extends SolidApi {
     // this.getItemLinks = link.getItemLinks
   }
 
-  /* START OF SESSION FUNCTIONS */
-
-  // TBD: Clarify if this is for solid-auth-cli only
-  /**
-   * Redirect the user to a login page if in the browser
-   * or login directly from command-line or node script
-   *
-   * @param {LoginCredentials} credentials
-   * @returns {Promise<Session>}
-   */
-  async login (credentials) {
-    let session = await this._auth.currentSession()
-    if (!session) {
-      session = await this._auth.login(credentials)
-    }
-    return session.webId
-  }
-
-  // TBD: What happens if the session returned by popupLogin is undefined?
-  //      (I guess this happens when the user just closes the popup. Maybe it also rejects in this case)
-  /**
-   * Open a popup prompting the user to login
-   * @param {string} [popupUri]
-   * @returns {Promise<string>} resolves with the webId after a
-   * successful login
-   */
-  async popupLogin (popupUri = defaultPopupUri) {
-    let session = await this._auth.currentSession()
-    if (!session) {
-      if (typeof window === 'undefined') {
-        session = await this._auth.login(popupUri)
-      } else {
-        session = await this._auth.popupLogin({ popupUri })
-      }
-    }
-    return session.webId
-  }
-
-  /*  POSSIBLY NOT BACKWARDS-COMPATIBLE : now return webId not session
-          note currentSession() returns session
-               checkSession returns webId
-    */
-  /**
-   * Return the currently active webId if available
-   * @returns {Promise<Session|undefined>} session if logged in, else undefined
-   */
-  async checkSession () {
-    const session = await this._auth.currentSession()
-    if (session) return session.webId
-    else return undefined
-  }
-
-  /**
-   * Return the currently active session if available
-   * @returns {Promise<Session|undefined>} session if logged in, else undefined
-   */
-  async currentSession () {
-    return this._auth.currentSession()
-  }
-
-  // TBD: What type is fn? What type is returned?
-  /**
-     * Get credentials from the current session
-     * @param {any} fn
-     * @returns {object}
-     */
-  getCredentials (fn) {
-    return this._auth.getCredentials(fn)
-  }
-
-  /**
-   * Logout the user from the pod
-   * @returns {Promise<void>}
-   */
-  logout () {
-    return this._auth.logout()
-  }
-
-  /* END OF SESSION FUNCTIONS */
 
   /**
      * Fetch an item and return content as text,json,or blob as needed
@@ -187,6 +65,8 @@ class SolidFileClient extends SolidApi {
 
   async deleteFolder (url, options) { return super.deleteFolderRecursively(url) }
 
+
+  // TBD remove or replace with patchFile() method
   async updateFile (url, content, contentType) {
     return super.putFile(url, content, contentType)
   }
@@ -223,8 +103,6 @@ class SolidFileClient extends SolidApi {
 
   // TBD array of existings links
   async getLinks (url, options) { return super.getLinks(url, options) }
-
-  // TBD: rdf.query ....
 
   /**
    * fetchAndParse
