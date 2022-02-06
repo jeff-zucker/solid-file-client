a73f1b59f5442503f48a200635eac36e7a597088
Matthias Evering <me@evering.eu>
2022-02-06 16:11:58 +0100

    temporary status 2022-02-07

--- docs/examples/advanced/copy.js
+++ docs/examples/advanced/copy.js
@@ -10,52 +10,20 @@ console.log('SolidFileClient object initialized');
 //this seems to be the actual copy command
 const { MERGE: { REPLACE, KEEP_SOURCE, KEEP_TARGET } } = SolidFileClient;
 console.log('MERGE const');
-const setCopyStatus = isCopying => {
-            if (isCopying) {
-                $('.not-copying').hide()
-                $('.copying').show()
-                console.log('show copying');
-            } else {
-                $('.not-copying').show()
-                $('.copying').hide()
-                console.log('show not copying');
-            }
-        }
-        setCopyStatus(false)
 
-        const setLogStatus = showLogs => {
-            if (showLogs) {
-                $('.logs').show()
-                console.log('show logs');
-            } else {
-                $('.logs').hide()
-                console.log('hide logs');
-            }
-        }
-        const resetLogs = () => {
-          console.log('reset logs');
-            setLogStatus(false)
-            $('.logs').empty()
-            console.log('logs resetted');
-        }
-
-        console.log('codeblock main');
-        const addSuccessLog = msg => $('.logs').append(`<li class="list-group-item list-group-item-success">${msg}</li>`)
-        const addErrorLog = msg => $('.logs').append(`<li class="list-group-item list-group-item-danger">${msg}</li>`)
-        resetLogs()
-
-        console.log('get elements by id');
-        const fromInput = document.getElementById('src')
-        const destInput = document.getElementById('dest')
-        const createPathInput = document.getElementById('create-path')
-        const withAclInput = document.getElementById('with-acl')
-        const withMetaInput = document.getElementById('with-meta')
-        const getMergeInput = () => document.querySelector('input[name="merge-option"]:checked')
-
-        console.log('process form');
+console.log('process form');
         document.getElementById('copy-form').addEventListener('submit', async e => {
             e.preventDefault()
             console.log('submit event done');
+
+            console.log('get elements by id');
+        	const fromInput = document.getElementById('src')
+       		const destInput = document.getElementById('dest')
+        	const createPathInput = document.getElementById('create-path')
+        	const withAclInput = document.getElementById('with-acl')
+        	const withMetaInput = document.getElementById('with-meta')
+        	const getMergeInput = () => document.querySelector('input[name="merge-option"]:checked')
+
             const from = fromInput.value
             const to = destInput.value
             const createPath = createPathInput.checked
@@ -71,6 +39,12 @@ const setCopyStatus = isCopying => {
 
             console.log('set response');
             const responseToMsg = response => `${response.status} ${response.statusText} ${response.url}`
+
+            console.log('codeblock main // MOVED');
+            const addSuccessLog = msg => $('.logs').append(`<li class="list-group-item list-group-item-success">${msg}</li>`)
+            const addErrorLog = msg => $('.logs').append(`<li class="list-group-item list-group-item-danger">${msg}</li>`)
+            resetLogs()
+
             try {
               console.log('set copy status true');
                 setCopyStatus(true)
@@ -100,3 +74,32 @@ const setCopyStatus = isCopying => {
             console.log('set log status true');
             setLogStatus(true)
         })
+
+const setCopyStatus = isCopying => {
+            if (isCopying) {
+                $('.not-copying').hide()
+                $('.copying').show()
+                console.log('show copying');
+            } else {
+                $('.not-copying').show()
+                $('.copying').hide()
+                console.log('show not copying');
+            }
+        }
+        setCopyStatus(false)
+
+        const setLogStatus = showLogs => {
+            if (showLogs) {
+                $('.logs').show()
+                console.log('show logs');
+            } else {
+                $('.logs').hide()
+                console.log('hide logs');
+            }
+        }
+        const resetLogs = () => {
+          console.log('reset logs');
+            setLogStatus(false)
+            $('.logs').empty()
+            console.log('logs resetted');
+        }
