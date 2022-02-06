fd105fba8e0e05734b9f37c95a4a2309cb6b4db0
Matthias Evering <me@evering.eu>
2022-02-06 13:00:25 +0100

    make app verbose

--- docs/examples/advanced/copy.js
+++ docs/examples/advanced/copy.js
@@ -3,22 +3,22 @@
 //
 //code goes here
 //?
-
-async function main(session){
-
+console.log('copy.js execution');
 const auth = solidClientAuthentication.getDefaultSession();
 const fileClient = new SolidFileClient( auth, { enableLogging: true });
-
+console.log('SolidFileClient object initialized');
 //this seems to be the actual copy command
 const { MERGE: { REPLACE, KEEP_SOURCE, KEEP_TARGET } } = SolidFileClient;
-
+console.log('MERGE const');
 const setCopyStatus = isCopying => {
             if (isCopying) {
                 $('.not-copying').hide()
                 $('.copying').show()
+                console.log('show copying');
             } else {
                 $('.not-copying').show()
                 $('.copying').hide()
+                console.log('show not copying');
             }
         }
         setCopyStatus(false)
@@ -26,18 +26,25 @@ const setCopyStatus = isCopying => {
         const setLogStatus = showLogs => {
             if (showLogs) {
                 $('.logs').show()
+                console.log('show logs');
             } else {
                 $('.logs').hide()
+                console.log('hide logs');
             }
         }
         const resetLogs = () => {
+          console.log('reset logs');
             setLogStatus(false)
             $('.logs').empty()
+            console.log('logs resetted');
         }
+
+        console.log('codeblock main');
         const addSuccessLog = msg => $('.logs').append(`<li class="list-group-item list-group-item-success">${msg}</li>`)
         const addErrorLog = msg => $('.logs').append(`<li class="list-group-item list-group-item-danger">${msg}</li>`)
         resetLogs()
 
+        console.log('get elements by id');
         const fromInput = document.getElementById('src')
         const destInput = document.getElementById('dest')
         const createPathInput = document.getElementById('create-path')
@@ -45,8 +52,10 @@ const setCopyStatus = isCopying => {
         const withMetaInput = document.getElementById('with-meta')
         const getMergeInput = () => document.querySelector('input[name="merge-option"]:checked')
 
+        console.log('process form');
         document.getElementById('copy-form').addEventListener('submit', async e => {
             e.preventDefault()
+            console.log('submit event done');
             const from = fromInput.value
             const to = destInput.value
             const createPath = createPathInput.checked
@@ -60,13 +69,15 @@ const setCopyStatus = isCopying => {
             else if (mergeVal === 'keep-target')
                 merge = KEEP_TARGET
 
-
+            console.log('set response');
             const responseToMsg = response => `${response.status} ${response.statusText} ${response.url}`
             try {
+              console.log('set copy status true');
                 setCopyStatus(true)
                 resetLogs()
 
                 // Copy a file or folder
+                console.log('actual copy work');
                 const res = await fileClient.copy(from, to, {
                     merge,
                     createPath,
@@ -84,7 +95,8 @@ const setCopyStatus = isCopying => {
                     addErrorLog(err.message)
                 })
             }
+            console.log('set copy status false');
             setCopyStatus(false)
+            console.log('set log status true');
             setLogStatus(true)
         })
-}
