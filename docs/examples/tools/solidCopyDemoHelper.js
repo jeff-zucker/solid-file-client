edb0c3ef2031eb18199887f42261fbb98862cc9e
Matthias Evering <me@evering.eu>
2022-02-07 15:46:06 +0100

    functionality!

--- docs/examples/tools/solidCopyDemoHelper.js
+++ docs/examples/tools/solidCopyDemoHelper.js
@@ -1,11 +1,18 @@
 // file docs/examples/tools/solidCopyDemoHelper.js
 // restructure february 2022 by @ewingson
 // helper class that fulfills the needed promise in solidCopyDemo.js
-function solidCopyDemoHelper(von,an,mer,cp,wacl,wmeta) {
+async function solidCopyDemoHelper(von,an,mer,cp,wacl,wmeta) {
 console.log('solidCopyDemoHelper code started');
 let antwort = 42;
+const authn = solidClientAuthentication.getDefaultSession();
+const fc = new SolidFileClient( authn, { enableLogging: true });
 
-
+/*let content = fc.copy(von, an, {
+    mer,
+    cp,
+    wacl,
+    wmeta
+})*/
 
 return antwort;
 }
