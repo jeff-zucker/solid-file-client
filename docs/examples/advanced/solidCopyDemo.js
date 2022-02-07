675efff5de88a9a62e1ff59d2788bb4356e12357
Matthias Evering <me@evering.eu>
2022-02-07 05:45:10 +0100

    a bit messed up, normalerweise nicht public

--- docs/examples/advanced/solidCopyDemo.js
+++ docs/examples/advanced/solidCopyDemo.js
@@ -6,7 +6,7 @@
 //codebase that is called at the bottom of advanced/index.html
 //import helper class
 import solidCopyDemoHelper from '../tools/solidCopyDemoHelper';
-console.log('file solidCopyDemo imports done');
+console.log('file solidCopyDemo import done');
 console.log('solidCopyDemo.js code execution');
 const auth = solidClientAuthentication.getDefaultSession();
 const fileClient = new SolidFileClient( auth, { enableLogging: true });
@@ -18,68 +18,7 @@ console.log('MERGE const');
 
 //split up L54
 
-console.log('process form');
-        document.getElementById('copy-form').addEventListener('submit', async e => {
-            e.preventDefault()
-            console.log('submit event done');
 
-            console.log('get elements by id');
-        	const fromInput = document.getElementById('src')
-       		const destInput = document.getElementById('dest')
-        	const createPathInput = document.getElementById('create-path')
-        	const withAclInput = document.getElementById('with-acl')
-        	const withMetaInput = document.getElementById('with-meta')
-        	const getMergeInput = () => document.querySelector('input[name="merge-option"]:checked')
-
-            const from = fromInput.value
-            const to = destInput.value
-            const createPath = createPathInput.checked
-            const withMeta = withMetaInput.checked
-            const withAcl = withAclInput.checked
-
-            const mergeVal = getMergeInput().value
-            let merge = REPLACE
-            if (mergeVal === 'keep-source')
-                merge = KEEP_SOURCE
-            else if (mergeVal === 'keep-target')
-                merge = KEEP_TARGET
-
-            console.log('set response');
-            //wo kommen diese 3 attribute/eigenschaften her ?
-            const responseToMsg = response => `${response.status} ${response.statusText} ${response.url}`
-            //console.log('status:' + ${response.status} + 'text:' + ${response.statusText} + 'url:' + ${response.url});
-//          resetLogs()
-//wird zweimal ausgeführt
-
-            try {
-              console.log('set copy status true');
-                setCopyStatus(true)
-                resetLogs()
-
-                // Copy a file or folder
-                console.log('actual copy work');
-                const res = await fileClient.copy(from, to, {
-                    merge,
-                    createPath,
-                    withAcl,
-                    withMeta
-                })
-                res.forEach(response => {
-                    const msg = responseToMsg(response)
-                    console.log(msg)
-                    addSuccessLog(msg)
-                })
-            } catch (err) {
-                err.rejectedErrors.forEach(err => {
-                    console.error(err)
-                    addErrorLog(err.message)
-                })
-            }
-            console.log('set copy status false');
-            setCopyStatus(false)
-            console.log('set log status true');
-            setLogStatus(true)
-        })
 
 const setCopyStatus = isCopying => {
             if (isCopying) {
@@ -111,8 +50,75 @@ const setCopyStatus = isCopying => {
         }
 
         console.log('codeblock main // MOVED');
-        const addSuccessLog = msg => $('.logs').append(`<li class="list-group-item list-group-item-success">${msg}</li>`)
-        const addErrorLog = msg => $('.logs').append(`<li class="list-group-item list-group-item-danger">${msg}</li>`)
         resetLogs()
-//console.log('solidCopyDemo export now');
-//export default solidCopyDemo;
+
+        console.log('process form');
+                document.getElementById('copy-form').addEventListener('submit', async e => {
+                    e.preventDefault()
+                    console.log('submit event done');
+
+                    console.log('get elements by id');
+                	const fromInput = document.getElementById('src')
+               		const destInput = document.getElementById('dest')
+                	const createPathInput = document.getElementById('create-path')
+                	const withAclInput = document.getElementById('with-acl')
+                	const withMetaInput = document.getElementById('with-meta')
+                	const getMergeInput = () => document.querySelector('input[name="merge-option"]:checked')
+
+                    const from = fromInput.value
+                    const to = destInput.value
+                    const createPath = createPathInput.checked
+                    const withMeta = withMetaInput.checked
+                    const withAcl = withAclInput.checked
+
+                    const mergeVal = getMergeInput().value
+                    let merge = REPLACE
+                    if (mergeVal === 'keep-source')
+                        merge = KEEP_SOURCE
+                    else if (mergeVal === 'keep-target')
+                        merge = KEEP_TARGET
+
+                    console.log(from + '_' + to + '_' + merge + '_' + createPath + '_' + withAcl + '_' + withMeta);
+
+                    console.log('set response');
+                    //console.log(solidCopyDemoHelper.testfirst);
+                    //wo kommen diese 3 attribute/eigenschaften her ?
+                    const responseToMsg = response => `${response.status} ${response.statusText} ${response.url}`
+
+                    //console.log('status:' + ${response.status} + 'text:' + ${response.statusText} + 'url:' + ${response.url});
+        //          resetLogs()
+        //wird zweimal ausgeführt
+
+                    try {
+                      console.log('set copy status true');
+                        setCopyStatus(true)
+                        resetLogs()
+
+                        // Copy a file or folder
+                        console.log('actual copy work');
+                        const res = await fileClient.copy(from, to, {
+                            merge,
+                            createPath,
+                            withAcl,
+                            withMeta
+                        })
+                        res.forEach(response => {
+                            const msg = responseToMsg(response)
+                            console.log(msg)
+                            const addSuccessLog = msg => $('.logs').append(`<li class="list-group-item list-group-item-success">${msg}</li>`)
+
+                            addSuccessLog(msg)
+                        })
+                    } catch (err) {
+                        err.rejectedErrors.forEach(err => {
+                            console.error(err)
+                            const addErrorLog = msg => $('.logs').append(`<li class="list-group-item list-group-item-danger">${msg}</li>`)
+
+                            addErrorLog(err.message)
+                        })
+                    }
+                    console.log('set copy status false');
+                    setCopyStatus(false)
+                    console.log('set log status true');
+                    setLogStatus(true)
+                })
--- docs/examples/tools/solidCopyDemoHelper.js
+++ docs/examples/tools/solidCopyDemoHelper.js
@@ -1,10 +1,19 @@
 // file docs/examples/tools/solidCopyDemoHelper.js
 // restructure february 2022 by @ewingson
 // helper class that fulfills the needed promise in solidCopyDemo.js
-async function solidCopyDemoHelper() {
+function solidCopyDemoHelper(von,an,mer,cp,wacl,wmeta) {
 console.log('solidCopyDemoHelper code started');
+/* console.log('msg:' + msg);
+console.log('from:' + from);
+console.log('to:' + to);
+console.log('merge:' + merge);
+console.log('createPath:' + createPath);
+console.log('withAcl:' + withAcl);
+console.log('withMeta:' + withMeta);
+*/
+const antwort = 'Test:' + Test;
 
-
+return antwort;
 
 }
 export default solidCopyDemoHelper;
